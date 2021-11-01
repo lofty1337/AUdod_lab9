85e34ff1ea574aebbb9749d4959425aa11ac2d2d
lofty1337 <90724908+lofty1337@users.noreply.github.com>
2021-11-01 16:59:26 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 ﻿#include <SFML/Graphics.hpp>
+#include <thread>
+#include <chrono>
+
+using namespace std::chrono_literals;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    float Cx,Cy,Sx,Sy,Ox,Oy;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "nge");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
-
+    Cx = 700;
+    Cy = 0;
+    shape.setPosition(Cx, Cy);
+    sf::CircleShape square(80.f, 4);
+    square.setFillColor(sf::Color::Blue);
+    Sx = 700;
+    Sy = 200;
+    square.setPosition(Sx, Sy);
+    sf::CircleShape octagon(80.f, 8);
+    octagon.setFillColor(sf::Color::Red);
+    Ox = 700;
+    Oy = 400;
+    octagon.setPosition(Ox, Oy);
     while (window.isOpen())
     {
         sf::Event event;
@@ -17,7 +34,19 @@ int main()
 
         window.clear();
         window.draw(shape);
+        window.draw(square);
+        window.draw(octagon);
         window.display();
+        if (Ox > 0)
+            --Ox;
+        if (Cx > 0)
+            --Cx;
+        if (Sx > 0)
+            --Sx;
+        shape.setPosition(Cx, Cy);
+        square.setPosition(Sx, Sy);
+        octagon.setPosition(Ox, Oy);
+        std::this_thread::sleep_for(0.1ms);
     }
 
     return 0;
